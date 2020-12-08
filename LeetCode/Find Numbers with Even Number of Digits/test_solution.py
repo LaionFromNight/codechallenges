29ece68ee61eb00549940e7daded849fd9d43cc2
lukas <komur.l@wp.pl>
2020-12-08 15:40:47 +0100

    Changes: refactor naming convention for Class and function in TestCases

--- LeetCode/Find Numbers with Even Number of Digits/test_solution.py
+++ LeetCode/Find Numbers with Even Number of Digits/test_solution.py
@@ -3,13 +3,13 @@ from parameterized import parameterized
 from solution import Solution
 
 
-class TestLeetCode_max_consecutive_onse(unittest.TestCase):
+class TestLeetCode(unittest.TestCase):
 
     @parameterized.expand([
         ([12,345,2,6,7896], 2),
         ([555,901,482,1771], 1)
     ])
-    def test_csv_helper_generate_headers_for_output_multiple(self, input, expect_result):
+    def test_multiple(self, input, expect_result):
         # Assume
         test = Solution()
 
--- LeetCode/Max Consecutive Ones/test_solution.py
+++ LeetCode/Max Consecutive Ones/test_solution.py
@@ -3,13 +3,13 @@ from parameterized import parameterized
 from solution import Solution
 
 
-class TestLeetCode_max_consecutive_onse(unittest.TestCase):
+class TestLeetCode(unittest.TestCase):
 
     @parameterized.expand([
         ([1, 1, 0, 1, 1, 1], 3),
         ([1,0,1,1,0,1], 2)
     ])
-    def test_csv_helper_generate_headers_for_output_multiple(self, input, expect_result):
+    def test_multiple(self, input, expect_result):
         # Assume
         test = Solution()
 
